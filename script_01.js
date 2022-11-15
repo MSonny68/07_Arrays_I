d62d041b07e4508f937a5da5913efa9444aa1bf8
MSonny68 <maik.sonnenberg@hotmail.degit config --global user.name MSonny68git loggit config --global user.email maik.sonnenberg@hotmail.de>
2022-11-15 12:27:06 +0100

    leerzeichen und punkt zufügebn

--- script_01.js
+++ script_01.js
@@ -13,6 +13,63 @@
 // output(arr[3]);
 // output(arr[arr.length-1]);
 
+/*** 02a. Theorie: Schleifen (for Schleife) */
+// for (let i = 0; i < 4; i++) {
+//     output("Index i:" + i);
+    
+// }
+
+/*
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 2 + 1
+// output("inhalt von a: " + a);
+
+/* Besser: mit For-Schleife */
+// let a = 0;
+// for (let i = 0; i < 10; i++) {
+//     output("im Loop:" +a);
+//     a +=1;
+    
+// }
+// output("nach Loop: " + a);
+
+/*** 02b. Funktionalität mit arrays 2 mit For-Schleife*/
+
+let arr = ["Ich","bin","der","coole", "Maik","Sonny"]
+//getSentenceArr2(arr);
+output(getSentenceArr2(arr));
+
+function getSentenceArr2(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+
+    let str = "";
+    for (let i = 0; i < arr.length; i++) {
+        //output(arr[i]);
+        str = str + arr[i] + GAP;
+        
+    }
+    str = str.trim();
+    str = str + PUNCT;
+
+    return str;
+}
+
+
+
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 
@@ -39,7 +96,7 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
 // arr[0],arr[1],... :: Numerische Struktur
 // --> Transformation semantisches Problem --> numerisches Problem
 
-output(getSentenceArr(["Ich","bin","der","coole", "Maik","Sonny"]))
+//output(getSentenceArr(["Ich","bin","der","coole", "Maik","Sonny"]))
 function getSentenceArr(arr) {
     const GAP = " ";
     const PUNCT = ".";
@@ -55,22 +112,7 @@ function getSentenceArr(arr) {
 }
 
 
-/*** 01b. Funktionalität mit arrays mit For-Schleife*/
 
-output(getSentenceArr(["Ich","bin","der","coole", "Maik","Sonny"]))
-function getSentenceArr(arr) {
-    const GAP = " ";
-    const PUNCT = ".";
-    let str =   arr[0] + GAP + 
-                arr[1] + GAP +
-                arr[2] + GAP +
-                arr[3] + GAP +
-                arr[4] + GAP +
-                arr[5] +
-                PUNCT;
-
-    return str;
-}
 
 
 
